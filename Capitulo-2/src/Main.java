d1b8b502ffea5c82638bd0095cf5af256ff365e5
Marcel017 <126120219+Marcel017@users.noreply.github.com>
2026-01-05 14:05:38 -0300

    feat: adiciona solucao do exercicio DooBee (Cap 1)" -m "Exercicio focado em fixar a sintaxe do loop While e condicional If. Output esperado: DooBeeDooBeeDo.

---
+++ Capitulo-2/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    static void main() {
+        int x = 1;
+
+        // Enquanto x for menor que 3 (vai rodar quando x for 1 e 2)
+        while (x < 3) {
+            System.out.println("Doo");
+            System.out.println("Bee");
+            x = x + 1; // Aumenta o x.
+        }
+
+        // Quando o loop acaba, x vale 3
+        if (x == 3) {
+            System.out.println("Do");
+        }
+    }
+}
